4b49e2030dfb906cf093ff72753d7a66089c96c8
Eugene-Olok <eugeneolok@gmail.com>
2026-08-07 20:48:27 +0300

    Update for wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/about': '/#about',
   },
+
+  adapter: cloudflare(),
 });
