ba7cc546bdbf4748b14727553d302b8af35302d4
Eugene-Olok <eugeneolok@gmail.com>
2026-08-11 13:36:23 +0300

    Changes to site configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://my-site.eugeneolok.workers.dev',
   redirects: {
     '/about': '/#about',
   },
-
-  adapter: cloudflare(),
 });
